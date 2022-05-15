c5f56f80de469898a887efc62f99eb808399d814
Ko3JIuHa <nikitabrauner@yandex.ru>
2022-05-15 11:33:44 +0300

    inite

---
+++ loader/5a966f9d286688e51e8b45c7.js
@@ -0,0 +1,11 @@
+// @copyright MegaGroup © 2012 - 2018, megagroup.ru
+// 2021-11-11 14:48:47
+// D: false 13 13
+(function (o,n,c) {
+  'use strict';
+  o[c] = o[c] || function() {(o[c].x=o[c].x||[]).push(arguments)};
+  var s = n.createElement('script'); s.type = 'text/javascript'; s.charset="utf-8";
+  s.async = true; s.src = "https://client.onicon.ru/visitor.js?v=4.4.89";
+  o[c]('init', {ver:'4.4.89',lang:'ru',staticClient: 'https://client.onicon.ru',siteId:'5a966f9d286688e51e8b45c7',server:'wss://n9.onicon.ru',initTime:new Date()});
+  n.getElementsByTagName('head')[0].appendChild(s);
+})(window, document, 'Onicon');
