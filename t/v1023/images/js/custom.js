96d631db36b5ec7d62d35e67852ea49e3cf3ff73
Arsipa <senya.suvorov.2003@mail.ru>
2022-05-15 15:47:27 +0300

    add header includes

--- t/v1023/images/js/custom.js
+++ t/v1023/images/js/custom.js
@@ -1,22 +1,15 @@
 window.onload = function () {
-    // if (document.querySelector("footer")) {
-    //     fetch("footer.html")
-    //         .then((response) => {
-    //             return response.text();
-    //         })
-    //         .then((data) => {
-    //             document.querySelector("footer").innerHTML = data;
-    //             const date = new Date();
-    //             document.getElementById(
-    //                 "footer-text"
-    //             ).innerText = `© 2000 - ${date.getFullYear()} эл\\д Планерное. Все права защищены`;
-    //             console.log(document.getElementById(
-    //                 "footer-text"
-    //             ));
-    //         });
-    // }
+    if (document.querySelector("header")) {
+        fetch("/inc/header.html")
+            .then((response) => {
+                return response.text();
+            })
+            .then((data) => {
+                document.querySelector("header").innerHTML = data;
+            });
+    }
     if (document.querySelector("footer")) {
-        fetch("/footer.html")
+        fetch("/inc/footer.html")
             .then((response) => {
                 return response.text();
             })
@@ -26,9 +19,6 @@ window.onload = function () {
                 document.getElementById(
                     "footer-text"
                 ).innerText = `© 2000 - ${date.getFullYear()} эл\\д Планерное. Все права защищены`;
-                console.log(document.getElementById(
-                    "footer-text"
-                ));
             });
     }
 };
